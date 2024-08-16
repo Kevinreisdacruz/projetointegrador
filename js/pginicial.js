97ba3c78968b9c998fa8f6cd21ae452e021edf16
KEVIN REIS DA CRUZ <kevin.rcruz@senacsp.edu.br>
2024-08-16 16:50:28 -0300

    feito um popup pro login do usuario e adicionado um user icon ao lado do imput da navbar

--- js/pginicial.js
+++ js/pginicial.js
@@ -1,1 +1,11 @@
+var caixalogin = document.getElementById('container-login');
+var login = document.getElementById('login');
+login.addEventListener("click",abrir)
+
+function abrir(){
+    caixalogin.style.display = "flex"
+    
+}
+
 AOS.init()
+
