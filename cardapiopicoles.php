dca2800c266fb4be3c0a75867c8a8b5c226bf5b3
KevinReis <kevinreisdacruz10@gmailcom>
2024-10-07 14:40:55 -0300

    nenhuma troca importante

--- cardapiopicoles.php
+++ cardapiopicoles.php
@@ -8,7 +8,7 @@
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
   <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/cardapiopicoles.css">
+  <link rel="stylesheet" href="css/cardapioproduto.css">
 </head>
 
 <body>
