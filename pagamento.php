845032780566c29f8a9e904d125c725545df9b04
KevinReis <kevinreisdacruz10@gmailcom>
2024-10-18 15:38:49 -0300

    arrumando o pagamento cartao

--- pagamento.php
+++ pagamento.php
@@ -1,17 +1,30 @@
 <?php
-    include'navbar.php'
+include 'navbar.php'
 ?>
-    <div class="box">
-        <div class="fundo-forma shadow-lg">
-            <h1>ESCOLHA SUA FORMA DE PAGAMENTO</h1>
-            <div class="imagens">
-                <div class="img-cartao">
-                    <img class="pagamento" src="imagelogos/cartoes.png" alt="">
-                </div>
-                <a href="pagamentocartao.html">
-                    <img class="pagamento" src="imagelogos/pix.png  " alt="">
-                </a>
-                <img class="pagamento" src="imagelogos/money-icon-911ee8.webp" alt="" style="width: 140px;">
+<div class="box">
+    <div class="fundo-forma shadow-lg">
+        <h1>ESCOLHA SUA FORMA DE PAGAMENTO</h1>
+        <div class="imagens">
+            <div class="img-cartao">
+                <img class="pagamento" src="imagelogos/cartoes.png" alt="">
             </div>
+            <a href="pagamentocartao.html">
+                <img class="pagamento" src="imagelogos/pix.png  " alt="">
+            </a>
+            <img class="pagamento" src="imagelogos/money-icon-911ee8.webp" alt="" style="width: 140px;">
         </div>
     </div>
+</div>
+
+<script src='https://code.jquery.com/jquery-3.7.1.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js'></script>
+<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
+    integrity='sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz'
+    crossorigin='anonymous'></script>
+    <script src="js/pginicial.js"></script>
+<script src="./js/pagamento.js"></script>
+
+
+</body>
+
+</html>
