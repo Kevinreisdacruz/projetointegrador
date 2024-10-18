ddfc55d7a986b1b9bd2b656a3b4621c5c6081980
KevinReis <kevinreisdacruz10@gmailcom>
2024-10-18 01:37:09 -0300

    aplicandoo include

--- footer.php
+++ footer.php
@@ -1,33 +1,13 @@
-<div class='footer'>
-    <div class='footer-interior'>
-      <h1 class='titulocontatos'>NOSSOS CONTATOS E SERVIÇOS</h1>
-      <a href='https://www.instagram.com/accounts/login/'>
-        <img src='imagelogos/instagram.png' class='rede-social' alt=''>
-      </a>
+<script src='https://code.jquery.com/jquery-3.7.1.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js'></script>
+<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
+  integrity='sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz'
+  crossorigin='anonymous'></script>
+  <script src='js/pginicial.js'></script>
+  <script src="produtos.js"></script>
+</body>
 
-      <a href='https://www.facebook.com/?locale=pt_BR'>
-        <img src='imagelogos/facebook2.png' class='rede-social' alt=''>
-      </a>
-
-      <h4 class='contatos'>TELEFONE:(19)99892-8999</h4>
-      <h4 class='contatos'>realegelatosrc@gmail.com</h4>
-      <h3 class='contatos'>Abertos de segunda-Feira a sábado <br> Das 12:00h até as 21:00h <br> Entregas só até as
-        17:00h
-      </h3>
-    </div>
-  </div>
-
-
-  <script src='https://code.jquery.com/jquery-3.7.1.js'></script>
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js'></script>
- <script src="js/pginicial.js"></script>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
- <script src="js/pagamentocard.js"></script>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-
- </body>
- </html>
+</html>
 
 
   
