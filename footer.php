91bc81ab7c13808ad83f4caac66f5a10585f276a
KEVIN REIS DA CRUZ <kevin.rcruz@senacsp.edu.br>
2024-12-05 13:52:24 -0300

    foote novo arrumado

--- footer.php
+++ footer.php
@@ -1,30 +1,50 @@
-<div class="footer">
-  <div class="footer-interior">
-    <h1 class="titulocontatos">NOSSOS CONTATOS E SERVIÇOS</h1>
-    <a style="text-decoration: none;" href="https://www.instagram.com/accounts/login/">
-      <img src="imagelogos/instagram.png" class="rede-social" alt="">
-    </a>
+<div class="box3">
+    <div class="interior">
+        <div class="aside-left">
+            HORARIO DE FUNCIONAMENTO: <br><br>Abertos de segunda-Feira a sábado <br> Das 12:00h até as 21:00h <br> Entregas só até as 17:00h
+        </div>
 
-    <a style="text-decoration: none;" href="https://www.facebook.com/?locale=pt_BR">
-      <img src="imagelogos/facebook2.png" class="rede-social" alt="">
-    </a>
+        <div class="holder">
+            <div class="redes">
+                <div class="center-content">
+                    <img src="imagelogos/logo.png" alt="">
 
-    <h4 class="contatos">TELEFONE:(19)99892-8999</h4>
-    <h4 class="contatos">realegelatosrc@gmail.com</h4>
-    <h3 class="contatos">Abertos de segunda-Feira a sábado <br> Das 12:00h até as 21:00h <br> Entregas só até as
-      17:00h
-    </h3>
-  </div>
+
+                </div>
+                <div class="redes-sociais">
+                    <a href="https://www.instagram.com/accounts/login/" target="_blank">
+                        <img class="rede" src="imagelogos/instagram.png" alt="">
+
+                    </a>
+
+                    <a href="https://www.facebook.com/?locale=pt_BR" target="_blank">
+                        <img class="rede" src="imagelogos/facebook2.png" alt="">
+                    </a>
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+
+        <div class="aside-right">
+            ENTRE EM CONTATO: <br> Telefone: (19)99892-8999 <br> E-mail: realegelatosrc@gmail.com
+        </div>
+
+
+    </div>
 </div>
-
 
 <script src='https://code.jquery.com/jquery-3.7.1.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js'></script>
 <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
-  integrity='sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz'
-  crossorigin='anonymous'></script>
+    integrity='sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz'
+    crossorigin='anonymous'></script>
 <script src="js/pginicial.js"></script>
 <script src="js/pagamento.js"></script>
+
 
 </body>
 
